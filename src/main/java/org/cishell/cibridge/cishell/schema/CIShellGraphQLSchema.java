c8b1dad4e4b1d5f5ff90ef4abad463acefe2a632
Nawaz <nawazkh@iu.edu>
2018-07-13 11:38:16 -0400

    one line changed for CIShell

--- src/main/java/org/cishell/cibridge/cishell/schema/CIShellGraphQLSchema.java
+++ src/main/java/org/cishell/cibridge/cishell/schema/CIShellGraphQLSchema.java
@@ -4,6 +4,7 @@ import org.cishell.cibridge.mock.resolvers.Mutation;
 import org.cishell.cibridge.model.Scalars;
 import org.cishell.cibridge.mock.data.AlgorithmDefinationMock;
 import org.cishell.cibridge.mock.resolvers.Query;
+import org.cishell.cibridge.mock.resolvers.Subscription;
 
 import com.coxautodev.graphql.tools.SchemaParser;
 
@@ -13,7 +14,7 @@ public class CIShellGraphQLSchema {
 	public static GraphQLSchema buildSchema() {
         GraphQLSchema gs = SchemaParser.newParser()
                 .file("cibridge-schema_0.1.0-draft.graphqls") //parse the schema file created earlier
-                .resolvers(new Query(new AlgorithmDefinationMock()))
+                .resolvers(new Query(new AlgorithmDefinationMock()),new Mutation(new AlgorithmDefinationMock()),new Subscription(new AlgorithmDefinationMock()))
                 .scalars(Scalars.date,Scalars.File,Scalars.Value)
                 .build()
                 .makeExecutableSchema();
