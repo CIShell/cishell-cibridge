54f3f6542924d8c5d478d52d6d278993621126f5
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-07-23 15:11:02 -0400

    adds mock impl for scheduler query & mutation resolvers

--- mock/src/main/java/org/cishell/cibridge/mock/impl/CIShellCIBridgeSchedulerFacade.java
+++ mock/src/main/java/org/cishell/cibridge/mock/impl/CIShellCIBridgeSchedulerFacade.java
@@ -1,63 +1,57 @@
 package org.cishell.cibridge.mock.impl;
 
 import org.cishell.cibridge.core.CIBridge;
+import org.cishell.cibridge.mock.data.Scheduler;
 
 import java.time.ZonedDateTime;
 
 public class CIShellCIBridgeSchedulerFacade implements CIBridge.SchedulerFacade {
 
+	private Scheduler scheduler = new Scheduler();
+
 	@Override
 	public Boolean isSchedulerEmpty() {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.getQueueWaiting() == 0;
 	}
 
 	@Override
 	public Boolean isSchedulerRunning() {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.getRunning();
 	}
 
 	@Override
 	public Integer getSchedulerQueueWaiting() {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.getQueueWaiting();
 	}
 
 	@Override
 	public Boolean runAlgorithmNow(String algorithmInstanceId) {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.runAlgorithmNow(algorithmInstanceId);
 	}
 
 	@Override
 	public Boolean scheduleAlgorithm(String algorithmInstanceId, ZonedDateTime date) {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.scheduleAlgorithm(algorithmInstanceId, date);
 	}
 
 	@Override
 	public Boolean rescheduleAlgorithm(String algorithmInstanceId, ZonedDateTime date) {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.rescheduleAlgorithm(algorithmInstanceId, date);
 	}
 
 	@Override
 	public Boolean unscheduleAlgorithm(String algorithmInstanceId) {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.unscheduleAlgorithm(algorithmInstanceId);
 	}
 
 	@Override
 	public Integer clearScheduler() {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.clearScheduler();
 	}
 
 	@Override
 	public Boolean setSchedulerRunning(Boolean running) {
-		// TODO Auto-generated method stub
-		return null;
+		return scheduler.setRunning(running);
 	}
 
 	@Override
