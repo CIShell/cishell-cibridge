de7c9b13266b55f8b85d9e9475fac38011219545
Bruce Herr II <bherr@indiana.edu>
2018-08-06 16:10:42 -0400

    Added bundle activator for deploying cibridge inside an osgi instance

---
+++ cishell/src/main/java/org/cishell/cibridge/cishell/graphql/CIBridgeServletActivator.java
@@ -0,0 +1,46 @@
+package org.cishell.cibridge.cishell.graphql;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.service.http.HttpService;
+
+import org.cishell.cibridge.cishell.graphql.CIBridgeServlet;
+
+
+public class CIBridgeServletActivator implements BundleActivator {
+    private ServiceTracker httpTracker;
+  
+    public void start(BundleContext context) throws Exception {
+      httpTracker = new ServiceTracker(context, HttpService.class.getName(), null) {
+        public void removedService(ServiceReference reference, Object service) {
+          // HTTP service is no longer available, unregister our servlet...
+          try {
+             ((HttpService) service).unregister("/graphql");
+          } catch (IllegalArgumentException exception) {
+             // Ignore; servlet registration probably failed earlier on...
+          }
+        }
+  
+        public Object addingService(ServiceReference reference) {
+          // HTTP service is available, register our servlet...
+          HttpService httpService = (HttpService) this.context.getService(reference);
+          try {
+            httpService.registerServlet("/graphql", new CIBridgeServlet(), null, null);
+          } catch (Exception exception) {
+            exception.printStackTrace();
+          }
+          return httpService;
+        }
+      };
+      // start tracking all HTTP services...
+      httpTracker.open();
+    }
+  
+    public void stop(BundleContext context) throws Exception {
+      // stop tracking all HTTP services...
+      httpTracker.close();
+    }
+  }
