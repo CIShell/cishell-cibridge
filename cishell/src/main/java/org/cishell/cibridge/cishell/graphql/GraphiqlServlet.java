ee9bc948bc457f47700167e888dbf2053f228ac0
Bruce Herr II <bherr@indiana.edu>
2018-08-06 16:25:17 -0400

    Add graphiql servlet

---
+++ cishell/src/main/java/org/cishell/cibridge/cishell/graphql/GraphiqlServlet.java
@@ -0,0 +1,48 @@
+package org.cishell.cibridge.cishell.graphql;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+
+public class GraphiqlServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+            response.setContentType("text/html");
+
+            PrintWriter out = response.getWriter();
+
+            out.println(GraphiqlServlet.graphiqlHTML);
+
+            out.close();
+    }
+
+    private static final String graphiqlHTML = GraphiqlServlet.entryToString("/graphiql.html");
+
+    private static String entryToString(String entryPath) {
+        try {
+            InputStream inputStream = GraphiqlServlet.class.getResourceAsStream(entryPath);
+            final int bufferSize = 1024;
+            final char[] buffer = new char[bufferSize];
+            final StringBuilder out = new StringBuilder();
+            Reader in = new InputStreamReader(inputStream, "UTF-8");
+            for (;;) {
+                int rsz = in.read(buffer, 0, buffer.length);
+                if (rsz < 0)
+                    break;
+                out.append(buffer, 0, rsz);
+            }
+            return out.toString();
+        } catch(Exception e) {
+            return "";
+        }
+    }
+}
+
