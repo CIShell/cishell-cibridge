e95ef7a64810153e9673e81c53d0b66ab87dd818
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-11-10 13:14:34 -0500

    [64] add unit tests for removeData() and uploadData()

--- cishell/src/test/java/org/cishell/cibridge/cishell/impl/CIShellCIBridgeDataFacadeIT.java
+++ cishell/src/test/java/org/cishell/cibridge/cishell/impl/CIShellCIBridgeDataFacadeIT.java
@@ -5,22 +5,33 @@ import org.cishell.cibridge.core.model.Data;
 import org.cishell.cibridge.core.model.DataProperties;
 import org.cishell.cibridge.core.model.DataType;
 import org.cishell.cibridge.core.model.PropertyInput;
+import org.junit.After;
 import org.junit.Test;
 
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class CIShellCIBridgeDataFacadeIT extends IntegrationTestCase {
 
+    @Test(expected = IllegalStateException.class)
+    public void uploadNonExistentFile() {
+        getCIShellCIBridge().cishellData.uploadData("SomeNonExistentFile.txt", null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void uploadDirectory() {
+        getCIShellCIBridge().cishellData.uploadData(System.getProperty("user.dir"), null);
+    }
+
     @Test
-    public void uploadData() {
+    public void uploadFile() {
         URL dataFileUrl = getClass().getClassLoader().getResource("sample.txt");
         assertNotNull(dataFileUrl);
         Data data = getCIShellCIBridge().cishellData.uploadData(dataFileUrl.getFile(), null);
+
         assertNotNull(data);
         assertNotNull(data.getId());
         assertNotNull(data.getFormat());
@@ -29,13 +40,10 @@ public class CIShellCIBridgeDataFacadeIT extends IntegrationTestCase {
         assertEquals("file-ext:txt", data.getCIShellData().getFormat());
 
         assertEquals(data.getCIShellData(), getDataManagerService().getAllData()[0]);
-
-        getDataManagerService().removeData(getDataManagerService().getAllData()[0]);
-        assertEquals(0, getDataManagerService().getAllData().length);
     }
 
     @Test
-    public void uploadDataWithProperties() {
+    public void uploadFileWithProperties() {
         String format = "file:text/csv";
         String label = "Research papers";
         String name = "Research papers by Laszlo Barabasi";
@@ -53,7 +61,6 @@ public class CIShellCIBridgeDataFacadeIT extends IntegrationTestCase {
         dataProperties.setType(dataType);
         dataProperties.setOtherProperties(otherProperties);
         Data data = getCIShellCIBridge().cishellData.uploadData(dataFileUrl.getFile(), dataProperties);
-        System.out.println(data);
 
         assertNotNull(data);
         assertNotNull(data.getId());
@@ -64,16 +71,33 @@ public class CIShellCIBridgeDataFacadeIT extends IntegrationTestCase {
         assertEquals(name, data.getName());
         assertEquals(dataType, data.getType());
         assertEquals(customProperty.getKey(), data.getOtherProperties().get(0).getKey());
-
         assertEquals(format, data.getCIShellData().getFormat());
         //todo check properties in cishell data
 
         assertEquals(data.getCIShellData(), getDataManagerService().getAllData()[0]);
+    }
 
-        getDataManagerService().removeData(getDataManagerService().getAllData()[0]);
+    @Test
+    public void removeData() {
+        URL dataFileUrl = getClass().getClassLoader().getResource("sample.txt");
+        assertNotNull(dataFileUrl);
+        Data data = getCIShellCIBridge().cishellData.uploadData(dataFileUrl.getFile(), null);
+
+        boolean isSuccess = getCIShellCIBridge().cishellData.removeData(data.getId());
+        assertTrue(isSuccess);
         assertEquals(0, getDataManagerService().getAllData().length);
 
-        IntegrationTestCase integrationTestCase = new IntegrationTestCase() {
-        };
+        boolean isSuccess1 = getCIShellCIBridge().cishellData.removeData("SomeNonExistentId");
+        assertFalse(isSuccess1);
+    }
+
+    @After
+    public void tearDown() {
+        if (getDataManagerService().getAllData() != null && getDataManagerService().getAllData().length > 0) {
+            for (org.cishell.framework.data.Data data : getDataManagerService().getAllData()) {
+                getDataManagerService().removeData(data);
+            }
+        }
+        assertEquals(0, getDataManagerService().getAllData().length);
     }
 }
