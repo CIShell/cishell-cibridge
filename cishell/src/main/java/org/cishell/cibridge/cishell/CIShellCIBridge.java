30954e3af2d76c25ddab8e8b67dbc34179bdecdd
Hardik Rakholiya <hmrakholiya@gmail.com>
2019-01-09 18:38:03 -0500

    [61] Remove much of the crap from CIShellCIBridge and restoring to its original glory!!

--- cishell/src/main/java/org/cishell/cibridge/cishell/CIShellCIBridge.java
+++ cishell/src/main/java/org/cishell/cibridge/cishell/CIShellCIBridge.java
@@ -4,8 +4,6 @@ import org.cishell.app.service.datamanager.DataManagerService;
 import org.cishell.app.service.scheduler.SchedulerService;
 import org.cishell.cibridge.cishell.impl.*;
 import org.cishell.cibridge.core.CIBridge;
-import org.cishell.cibridge.core.model.AlgorithmDataObject;
-import org.cishell.cibridge.core.model.AlgorithmFactoryDataObject;
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.LocalCIShellContext;
 import org.cishell.framework.algorithm.AlgorithmFactory;
@@ -17,8 +15,6 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.log.LogService;
 import org.osgi.service.metatype.MetaTypeService;
 
-import java.util.HashMap;
-
 public class CIShellCIBridge extends CIBridge {
     private BundleContext context;
     private CIShellContext ciShellContext;
@@ -28,8 +24,6 @@ public class CIShellCIBridge extends CIBridge {
     public final CIShellCIBridgeNotificationFacade cishellNotification;
     public final CIShellCIBridgeSchedulerFacade cishellScheduler;
     public final CIShellCIBridgeLoggingFacade cishellLogging;
-    public final HashMap<String, AlgorithmDataObject> algorithmDataMap = new HashMap<>();
-    public final HashMap<String, AlgorithmFactoryDataObject> algorithmFactoryDataMap = new HashMap<>();
 
     public CIShellCIBridge(BundleContext context) {
         super(new CIShellCIBridgeAlgorithmFacade(), new CIShellCIBridgeDataFacade(),
@@ -50,26 +44,10 @@ public class CIShellCIBridge extends CIBridge {
         cishellScheduler.setCIBridge(this);
         cishellLogging.setCIBridge(this);
 
-        getBundleContext().addServiceListener(event -> {
-            if (event.getType() == ServiceEvent.REGISTERED) {
-                if (event.getServiceReference().toString().equals("[" + AlgorithmFactory.class.getName() + "]")) {
-                    ServiceReference ref = event.getServiceReference();
-                    cishellAlgorithm.cacheAlgorithm(ref);
-                }
-            } else if (event.getType() == ServiceEvent.UNREGISTERING) {
-                if (event.getServiceReference().toString().equals("[" + AlgorithmFactory.class.getName() + "]")) {
-                    ServiceReference ref = event.getServiceReference();
-                    cishellAlgorithm.uncacheAlgorithm(ref);
-                }
-            } else if (event.getType() == ServiceEvent.MODIFIED) {
-                //TODO
-            }
-        });
-
-        this.cishellAlgorithm.cacheAlgorithmData();
-        if (getSchedulerService() != null) {
-            getSchedulerService().addSchedulerListener(new SchedulerListenerImpl(this));
-        }
+        this.cishellAlgorithm.cacheAlgorithmDefinitions();
+//        if (getSchedulerService() != null) {
+//            getSchedulerService().addSchedulerListener(new SchedulerListenerImpl(this));
+//        }
 
     }
 
