bfa90888bb96d47a89c9ec871e972cc50cdb683b
Hardik Rakholiya <hmrakholiya@gmail.com>
2019-01-15 16:32:39 -0500

    [61] Add a generic class to store paginated results

---
+++ cishell/src/main/java/org/cishell/cibridge/cishell/impl/QueryResultsImpl.java
@@ -0,0 +1,27 @@
+package org.cishell.cibridge.cishell.impl;
+
+import org.cishell.cibridge.core.model.PageInfo;
+import org.cishell.cibridge.core.model.QueryResults;
+
+import java.util.List;
+
+//todo rename this class to something more useful
+public class QueryResultsImpl<T> implements QueryResults<T> {
+    private List<T> results;
+    private PageInfo pageInfo;
+
+    public QueryResultsImpl(List<T> results, PageInfo pageInfo) {
+        this.results = results;
+        this.pageInfo = pageInfo;
+    }
+
+    @Override
+    public List<T> getResults() {
+        return results;
+    }
+
+    @Override
+    public PageInfo getPageInfo() {
+        return pageInfo;
+    }
+}
