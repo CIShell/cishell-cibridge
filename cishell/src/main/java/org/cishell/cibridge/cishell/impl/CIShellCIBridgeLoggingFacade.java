5ad20752a57eea82344e26266edae32935040af4
Bharatha <abharath@ads.iu.edu>
2019-02-01 11:20:33 -0500

    Logging facade Implementation

--- cishell/src/main/java/org/cishell/cibridge/cishell/impl/CIShellCIBridgeLoggingFacade.java
+++ cishell/src/main/java/org/cishell/cibridge/cishell/impl/CIShellCIBridgeLoggingFacade.java
@@ -1,143 +1,143 @@
 package org.cishell.cibridge.cishell.impl;
 
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Predicate;
+
 import org.cishell.cibridge.cishell.CIShellCIBridge;
+import org.cishell.cibridge.cishell.util.PaginationUtil;
 import org.cishell.cibridge.core.CIBridge;
-import org.cishell.cibridge.core.model.*;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
+import org.cishell.cibridge.core.model.Log;
+import org.cishell.cibridge.core.model.LogFilter;
+import org.cishell.cibridge.core.model.LogLevel;
+import org.cishell.cibridge.core.model.LogQueryResults;
+import org.cishell.cibridge.core.model.QueryResults;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.log.LogEntry;
-import org.osgi.service.log.LogListener;
 import org.osgi.service.log.LogReaderService;
 
-import java.util.*;
-
+import com.google.common.base.Preconditions;
 
 public class CIShellCIBridgeLoggingFacade implements CIBridge.LoggingFacade {
-    private CIShellCIBridge cibridge;
-    private LogReaderService logReaderService;
-    private LogListener fileLogger;
-    private List<LogReaderService> logReaders;
-    private ServiceListener serviceListener;
+	private CIShellCIBridge cibridge;
 
-    public void setCIBridge(CIShellCIBridge cibridge) {
-        this.cibridge = cibridge;
-        this.logReaderService = getLogService();
-        this.logReaders = new ArrayList<LogReaderService>();
-        this.serviceListener = new ServiceListener() {
-            @Override
-            public void serviceChanged(ServiceEvent event) {
-                if (logReaderService != null) {
-                    if (event.getType() == ServiceEvent.REGISTERED) {
-                        logReaders.add(logReaderService);
-                        logReaderService.addLogListener(fileLogger);
-                    } else if (event.getType() == ServiceEvent.UNREGISTERING) {
-                        logReaderService.removeLogListener(fileLogger);
-                        logReaders.remove(logReaderService);
-                    }
-                }
-            }
-        };
-    }
+	public void setCIBridge(CIShellCIBridge cibridge) {
+		this.cibridge = cibridge;
+	}
 
-    @Override
-    public LogQueryResults getLogs(LogFilter filter) {
-        try {
+	@SuppressWarnings("unchecked")
+	@Override
+	public LogQueryResults getLogs(LogFilter filter) {
 
-            LogReaderService reader = getLogService();
-            List<Log> logs = new ArrayList<Log>();
-            LogQueryResults logResults = null;
-            HashSet<LogLevel> loglevelSet = new HashSet<LogLevel>();
-            PageInfo pageInfo = new PageInfo(false, false);
-            Enumeration<LogEntry> latestLogs = reader.getLog();
-            HashMap<Integer, LogLevel> logLevelMap = new HashMap<>();
-            int limit = 0, offset = 0;
+		Preconditions.checkNotNull(filter, "filter can't be empty");
 
-            logLevelMap.put(1, LogLevel.ERROR);
-            logLevelMap.put(2, LogLevel.WARNING);
-            logLevelMap.put(3, LogLevel.INFO);
-            logLevelMap.put(4, LogLevel.DEBUG);
+		LogQueryResults results = null;
+		ArrayList<LogEntry> listOfLogs;
+		List<Predicate<LogEntry>> criteria = new ArrayList<>();
+		HashMap<Integer, LogLevel> logLevelMap = new HashMap<>();
+		logLevelMap.put(1, LogLevel.ERROR);
+		logLevelMap.put(2, LogLevel.WARNING);
+		logLevelMap.put(3, LogLevel.INFO);
+		logLevelMap.put(4, LogLevel.DEBUG);
 
-            if (filter != null) {
+		try {
+			LogReaderService logReaderService = getLogReaderService();
+			listOfLogs = Collections.list(logReaderService.getLog());
 
-                for (LogLevel temp : filter.logLevel) {
-                    loglevelSet.add(temp);
-                }
-                while (latestLogs.hasMoreElements()) {
-                    LogEntry log = (LogEntry) latestLogs.nextElement();
-                    Log tempLog = new Log();
-                    if (logLevelMap.get(log.getLevel()) != null) {
-                        if (loglevelSet.contains(logLevelMap.get(log.getLevel()))) {
-                            tempLog.setLogLevel(logLevelMap.get(log.getLevel()));
-                            tempLog.setMessage(log.getMessage());
-                            logs.add(tempLog);
-                        }
-                    }
-                    //TODO Timestamp
-                    // if(filter.logsBefore!=null){
-                    // ZonedDateTime dateTime = Instant.ofEpochMilli(log.getTime())
-                    //.atZone(ZoneId.of("-5:00"));
-                    // tempLog.setTimestamp(dateTime);
-                    // if((dateTime.compareTo(filter.logsBefore))<=0)
-                    // 	logs.add(tempLog);
-                    // }
+			criteria.add(data -> {
+				if (data == null)
+					return false;
+				if (filter.getLogLevel() == null)
+					return true;
+				return filter.getLogLevel().contains(logLevelMap.get(data.getLevel()));
+			});
 
-                }
-                limit = filter.getLimit();
-                offset = filter.getOffset();
-            } else {
-                System.out.println("Filter is empty!");
-                while (latestLogs.hasMoreElements()) {
-                    LogEntry log = (LogEntry) latestLogs.nextElement();
-                    Log tempLog = new Log();
+			criteria.add(data -> {
+				if (data == null)
+					return false;
+				if (filter.getLogsBefore() == null)
+					return true;
+				else {
+					Instant i = Instant.ofEpochMilli(data.getTime());
+					ZonedDateTime logCreatedTIme = ZonedDateTime.ofInstant(i, ZoneOffset.systemDefault());
+					return filter.getLogsBefore().isAfter(logCreatedTIme);
+				}
 
-                    switch (log.getLevel()) {
-                        case 1:
-                            tempLog.setLogLevel(LogLevel.ERROR);
-                            break;
-                        case 2:
-                            tempLog.setLogLevel(LogLevel.WARNING);
-                            break;
-                        case 3:
-                            tempLog.setLogLevel(LogLevel.INFO);
-                            break;
-                        case 4:
-                            tempLog.setLogLevel(LogLevel.DEBUG);
-                            break;
-                        default:
-                            break;
-                    }
+			});
 
-                    tempLog.setMessage(log.getMessage());
-                    //TODO Timestamp
-                    // ZonedDateTime dateTime = Instant.ofEpochMilli(log.getTime())
-                    // 	.atZone(ZoneId.of("-5:00"));
-                    // tempLog.setTimestamp(dateTime);
-                    logs.add(tempLog);
-                }
-            }
-            logResults = new LogQueryResults(logs, pageInfo);
-            return (LogQueryResults) cibridge.getPaginatedQueryResults(logResults, limit, offset);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+			criteria.add(data -> {
+				if (data == null)
+					return false;
+				if (filter.getLogsSince() == null)
+					return true;
+				else {
+					Instant i = Instant.ofEpochMilli(data.getTime());
+					ZonedDateTime logCreatedTIme = ZonedDateTime.ofInstant(i, ZoneOffset.systemDefault());
+					return filter.getLogsSince().isBefore(logCreatedTIme);
+				}
 
-    //TODO
-    @Override
-    public Log logAdded(List<LogLevel> logLevels) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+			});
 
+			QueryResults<LogEntry> paginatedQueryResults = PaginationUtil.getPaginatedResults(listOfLogs, criteria,
+					filter.getOffset(), filter.getLimit());
 
-    private LogReaderService getLogService() {
-        ServiceReference ref = this.cibridge.getBundleContext().getServiceReference(LogReaderService.class.getName());
-        if (ref == null) {
-            throw new RuntimeException("The required OSGi LogService is not installed.");
-        } else {
-            return (LogReaderService) this.cibridge.getBundleContext().getService(ref);
-        }
-    }
+			List<Log> log = new ArrayList<Log>();
+			for (LogEntry ll : paginatedQueryResults.getResults()) {
+				Log temp = new Log();
+				temp.setLogLevel(logLevelMap.get(ll.getLevel()));
+				temp.setMessage(ll.getMessage());
+				List<String> stacktrace = new ArrayList<String>();
+				if (ll.getException() != null) {
+					StackTraceElement[] stackTraceArray = ll.getException().getStackTrace();
+					if (stackTraceArray != null) {
+						for (StackTraceElement e : stackTraceArray) {
+							stacktrace.add(e.toString());
+						}
+						temp.setStackTrace(stacktrace);
+					} else {
+						temp.setStackTrace(null);
+					}
+				} else {
+					temp.setStackTrace(null);
+				}
+				Instant i = Instant.ofEpochSecond(ll.getTime());
+				ZonedDateTime z = ZonedDateTime.ofInstant(i, ZoneOffset.UTC);
+				temp.setTimestamp(z);
+				log.add(temp);
+			}
+
+			return new LogQueryResults(log, paginatedQueryResults.getPageInfo());
+
+		} catch (Exception e) {
+			System.out.println("LogReader service returned null");
+			e.printStackTrace();
+		}
+
+		return results;
+
+	}
+
+	// TODO use log service listener for subscriptions
+	@Override
+	public Log logAdded(List<LogLevel> logLevels) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private LogReaderService getLogReaderService() {
+		ServiceReference<LogReaderService> ref = this.cibridge.getBundleContext()
+				.getServiceReference(LogReaderService.class);
+		if (ref == null) {
+			// TODO why are we throwing a runtime exception here? it could break the
+			// application.
+			throw new RuntimeException("The required OSGi LogService is not installed.");
+		} else {
+			return (LogReaderService) this.cibridge.getBundleContext().getService(ref);
+		}
+	}
 }
