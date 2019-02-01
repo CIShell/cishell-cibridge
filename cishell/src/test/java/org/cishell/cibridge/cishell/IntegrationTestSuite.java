7e271beac15d66cf662d9941a7a32c6f70e9437d
Hardik Rakholiya <hardikrakholiya@users.noreply.github.com>
2019-02-01 12:07:35 -0500

    Revert "Logging facade impl"

--- cishell/src/test/java/org/cishell/cibridge/cishell/IntegrationTestSuite.java
+++ cishell/src/test/java/org/cishell/cibridge/cishell/IntegrationTestSuite.java
@@ -2,14 +2,12 @@ package org.cishell.cibridge.cishell;
 
 import org.cishell.cibridge.cishell.impl.CIShellCIBridgeAlgorithmFacadeIT;
 import org.cishell.cibridge.cishell.impl.CIShellCIBridgeDataFacadeIT;
-import org.cishell.cibridge.cishell.impl.CIShellCIBridgeLoggingFacadeIT;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         CIShellContainerLauncher.class,
-        CIShellCIBridgeLoggingFacadeIT.class,
         CIShellCIBridgeDataFacadeIT.class,
         CIShellCIBridgeAlgorithmFacadeIT.class,
 })
